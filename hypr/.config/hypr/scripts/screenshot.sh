64d6bac024a41bd2090fbaa72ffb11418140f179
Kingmtu <Kingmtuumar@gmail.com>
2025-11-23 12:12:03 +0530

    update
    update

--- hypr/.config/hypr/scripts/screenshot.sh
+++ hypr/.config/hypr/scripts/screenshot.sh
@@ -1,224 +1,128 @@
-#!/bin/bash
-#  ____                               _           _
-# / ___|  ___ _ __ ___  ___ _ __  ___| |__   ___ | |_
-# \___ \ / __| '__/ _ \/ _ \ '_ \/ __| '_ \ / _ \| __|
-#  ___) | (__| | |  __/  __/ | | \__ \ | | | (_) | |_
-# |____/ \___|_|  \___|\___|_| |_|___/_| |_|\___/ \__|
-#
-# Based on https://github.com/hyprwm/contrib/blob/main/grimblast/screenshot.sh
-# -----------------------------------------------------
+#!/usr/bin/env bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# Screenshots scripts
 
-# Screenshots will be stored in $HOME by default.
-# The screenshot will be moved into the screenshot directory
+iDIR="$HOME/.config/swaync/icons"
+sDIR="."
+notify_cmd_shot="notify-send -h string:x-canonical-private-synchronous:shot-notify -u low -i ${iDIR}/picture.png"
 
-# Add this to ~/.config/user-dirs.dirs to save screenshots in a custom folder:
-# XDG_SCREENSHOTS_DIR="$HOME/Screenshots"
+time=$(date "+%d-%b_%H-%M-%S")
+dir="$(xdg-user-dir)/Pictures/Screenshots"
+file="Screenshot_${time}_${RANDOM}.png"
 
-prompt='Screenshot'
-mesg="DIR: ~/Screenshots"
+active_window_class=$(hyprctl -j activewindow | jq -r '(.class)')
+active_window_file="Screenshot_${time}_${active_window_class}.png"
+active_window_path="${dir}/${active_window_file}"
 
-# Screenshot Filename
-source ~/.config/ml4w/settings/screenshot-filename.sh
-
-# Screenshot Folder
-source ~/.config/ml4w/settings/screenshot-folder.sh
-
-# Screenshot Editor
-export GRIMBLAST_EDITOR="$(cat ~/.config/ml4w/settings/screenshot-editor.sh)"
-
-# Example for keybindings
-# bind = SUPER, p, exec, grimblast save active
-# bind = SUPER SHIFT, p, exec, grimblast save area
-# bind = SUPER ALT, p, exec, grimblast save output
-# bind = SUPER CTRL, p, exec, grimblast save screen
-
-# Options
-option_1="Immediate"
-option_2="Delayed"
-
-option_capture_1="Capture Everything"
-option_capture_2="Capture Active Display"
-option_capture_3="Capture Selection"
-
-option_time_1="5s"
-option_time_2="10s"
-option_time_3="20s"
-option_time_4="30s"
-option_time_5="60s"
-#option_time_4="Custom (in seconds)" # Roadmap or someone contribute :)
-
-list_col='1'
-list_row='2'
-
-copy='Copy'
-save='Save'
-copy_save='Copy & Save'
-edit='Edit'
-
-# Rofi CMD
-rofi_cmd() {
-    rofi -dmenu -replace -config ~/.config/rofi/config-screenshot.rasi -i -no-show-icons -l 2 -width 30 -p "Take screenshot"
-}
-
-# Pass variables to rofi dmenu
-run_rofi() {
-    echo -e "$option_1\n$option_2" | rofi_cmd
-}
-
-####
-# Choose Timer
-# CMD
-timer_cmd() {
-    rofi -dmenu -replace -config ~/.config/rofi/config-screenshot.rasi -i -no-show-icons -l 5 -width 30 -p "Choose timer"
-}
-
-# Ask for confirmation
-timer_exit() {
-    echo -e "$option_time_1\n$option_time_2\n$option_time_3\n$option_time_4\n$option_time_5" | timer_cmd
-}
-
-# Confirm and execute
-timer_run() {
-    selected_timer="$(timer_exit)"
-    if [[ "$selected_timer" == "$option_time_1" ]]; then
-        countdown=5
-        ${1}
-    elif [[ "$selected_timer" == "$option_time_2" ]]; then
-        countdown=10
-        ${1}
-    elif [[ "$selected_timer" == "$option_time_3" ]]; then
-        countdown=20
-        ${1}
-    elif [[ "$selected_timer" == "$option_time_4" ]]; then
-        countdown=30
-        ${1}
-    elif [[ "$selected_timer" == "$option_time_5" ]]; then
-        countdown=60
-        ${1}
+# notify and view screenshot
+notify_view() {
+    if [[ "$1" == "active" ]]; then
+        if [[ -e "${active_window_path}" ]]; then
+            ${notify_cmd_shot} "Screenshot of '${active_window_class}' Saved."
+            "${sDIR}/Sounds.sh" --screenshot
+        else
+            ${notify_cmd_shot} "Screenshot of '${active_window_class}' not Saved"
+        fi
+    elif [[ "$1" == "swappy" ]]; then
+		${notify_cmd_shot} "Screenshot Captured."
     else
-        exit
+        local check_file="$dir/$file"
+        if [[ -e "$check_file" ]]; then
+            ${notify_cmd_shot} "Screenshot Saved."
+            "${sDIR}/Sounds.sh" --screenshot
+        else
+            ${notify_cmd_shot} "Screenshot NOT Saved."
+        fi
     fi
 }
-###
 
-####
-# Chose Screenshot Type
-# CMD
-type_screenshot_cmd() {
-    rofi -dmenu -replace -config ~/.config/rofi/config-screenshot.rasi -i -no-show-icons -l 3 -width 30 -p "Type of screenshot"
-}
 
-# Ask for confirmation
-type_screenshot_exit() {
-    echo -e "$option_capture_1\n$option_capture_2\n$option_capture_3" | type_screenshot_cmd
-}
 
-# Confirm and execute
-type_screenshot_run() {
-    selected_type_screenshot="$(type_screenshot_exit)"
-    if [[ "$selected_type_screenshot" == "$option_capture_1" ]]; then
-        option_type_screenshot=screen
-        ${1}
-    elif [[ "$selected_type_screenshot" == "$option_capture_2" ]]; then
-        option_type_screenshot=output
-        ${1}
-    elif [[ "$selected_type_screenshot" == "$option_capture_3" ]]; then
-        option_type_screenshot=area
-        ${1}
-    else
-        exit
-    fi
-}
-###
-
-####
-# Choose to save or copy photo
-# CMD
-copy_save_editor_cmd() {
-    rofi -dmenu -replace -config ~/.config/rofi/config-screenshot.rasi -i -no-show-icons -l 4 -width 30 -p "How to save"
-}
-
-# Ask for confirmation
-copy_save_editor_exit() {
-    echo -e "$copy\n$save\n$copy_save\n$edit" | copy_save_editor_cmd
-}
-
-# Confirm and execute
-copy_save_editor_run() {
-    selected_chosen="$(copy_save_editor_exit)"
-    if [[ "$selected_chosen" == "$copy" ]]; then
-        option_chosen=copy
-        ${1}
-    elif [[ "$selected_chosen" == "$save" ]]; then
-        option_chosen=save
-        ${1}
-    elif [[ "$selected_chosen" == "$copy_save" ]]; then
-        option_chosen=copysave
-        ${1}
-    elif [[ "$selected_chosen" == "$edit" ]]; then
-        option_chosen=edit
-        ${1}
-    else
-        exit
-    fi
-}
-###
-
-timer() {
-    if [[ $countdown -gt 10 ]]; then
-        notify-send -t 1000 "Taking screenshot in ${countdown} seconds"
-        countdown_less_10=$((countdown - 10))
-        sleep $countdown_less_10
-        countdown=10
-    fi
-    while [[ $countdown -ne 0 ]]; do
-        notify-send -t 1000 "Taking screenshot in ${countdown} seconds"
-        countdown=$((countdown - 1))
-        sleep 1
-    done
+# countdown
+countdown() {
+	for sec in $(seq $1 -1 1); do
+		notify-send -h string:x-canonical-private-synchronous:shot-notify -t 1000 -i "$iDIR"/timer.png "Taking shot in : $sec"
+		sleep 1
+	done
 }
 
 # take shots
-takescreenshot() {
-    sleep 1
-    grimblast --notify "$option_chosen" "$option_type_screenshot" $NAME
-    if [ -f $HOME/$NAME ]; then
-        if [ -d $screenshot_folder ]; then
-            mv $HOME/$NAME $screenshot_folder/
-        fi
-    fi
+shotnow() {
+	cd ${dir} && grim - | tee "$file" | wl-copy
+	sleep 2
+	notify_view
 }
 
-takescreenshot_timer() {
-    sleep 1
-    timer
-    grimblast --notify "$option_chosen" "$option_type_screenshot" $NAME
-    if [ -f $HOME/$NAME ]; then
-        if [ -d $screenshot_folder ]; then
-            mv $HOME/$NAME $screenshot_folder/
-        fi
-    fi
+shot5() {
+	countdown '5'
+	sleep 1 && cd ${dir} && grim - | tee "$file" | wl-copy
+	sleep 1
+	notify_view
+	
 }
 
-# Execute Command
-run_cmd() {
-    if [[ "$1" == '--opt1' ]]; then
-        type_screenshot_run
-        copy_save_editor_run "takescreenshot"
-    elif [[ "$1" == '--opt2' ]]; then
-        timer_run
-        type_screenshot_run
-        copy_save_editor_run "takescreenshot_timer"
-    fi
+shot10() {
+	countdown '10'
+	sleep 1 && cd ${dir} && grim - | tee "$file" | wl-copy
+	notify_view
 }
 
-# Actions
-chosen="$(run_rofi)"
-case ${chosen} in
-    $option_1)
-        run_cmd --opt1
-        ;;
-    $option_2)
-        run_cmd --opt2
-        ;;
-esac
+shotwin() {
+	w_pos=$(hyprctl activewindow | grep 'at:' | cut -d':' -f2 | tr -d ' ' | tail -n1)
+	w_size=$(hyprctl activewindow | grep 'size:' | cut -d':' -f2 | tr -d ' ' | tail -n1 | sed s/,/x/g)
+	cd ${dir} && grim -g "$w_pos $w_size" - | tee "$file" | wl-copy
+	notify_view
+}
+
+shotarea() {
+	tmpfile=$(mktemp)
+	grim -g "$(slurp)" - >"$tmpfile"
+	if [[ -s "$tmpfile" ]]; then
+		wl-copy <"$tmpfile"
+		mv "$tmpfile" "$dir/$file"
+	fi
+	rm "$tmpfile"
+	notify_view
+}
+
+shotactive() {
+    active_window_class=$(hyprctl -j activewindow | jq -r '(.class)')
+    active_window_file="Screenshot_${time}_${active_window_class}.png"
+    active_window_path="${dir}/${active_window_file}"
+
+    hyprctl -j activewindow | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' | grim -g - "${active_window_path}"
+	sleep 1
+    notify_view "active"  
+}
+
+shotswappy() {
+	tmpfile=$(mktemp)
+	grim -g "$(slurp)" - >"$tmpfile" && "${sDIR}/Sounds.sh" --screenshot && notify_view "swappy"
+	swappy -f - <"$tmpfile"
+	rm "$tmpfile"
+}
+
+
+if [[ ! -d "$dir" ]]; then
+	mkdir -p "$dir"
+fi
+
+if [[ "$1" == "--now" ]]; then
+	shotnow
+elif [[ "$1" == "--in5" ]]; then
+	shot5
+elif [[ "$1" == "--in10" ]]; then
+	shot10
+elif [[ "$1" == "--win" ]]; then
+	shotwin
+elif [[ "$1" == "--area" ]]; then
+	shotarea
+elif [[ "$1" == "--active" ]]; then
+	shotactive
+elif [[ "$1" == "--swappy" ]]; then
+	shotswappy
+else
+	echo -e "Available Options : --now --in5 --in10 --win --area --active --swappy"
+fi
+
+exit 0
